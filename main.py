1ae9ac7264c0a97d33a316bfd5f6281dcf66674a
JR-JJ <jardiel0307@gmail.com>
2024-01-26 21:08:01 -0500

    Added function to print board.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+print('Welcome to our game of Tic-Tac-Toe!')
+print('Here is an initial view of the board with positions to choose from.')
+
 print('''
-    |    |
-____|____|____
-    |    |
-____|____|____
-    |    |
-    |    |
+        _1|_2|_3
+        _4|_5|_6
+         7| 8| 9
       ''')
+
+def update_board():
+    board = [['','',''],['','',''],['','','']]
+    return board
+
+def print_board(board):
+    for item in board:
+        print(item)
+
+print_board(update_board())
